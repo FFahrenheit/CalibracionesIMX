70b8bf313ba7fe1fe95cf515555f8d91a4acec2e
Iván L. Murillo <diskman199@gmail.com>
2021-05-27 09:39:58 -0500

    Agregado servicio de alerta (no probado)

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from './footer/footer.component';
+import { AlertComponent } from './alert/alert.component';
 
 @NgModule({
   declarations: [
+    AlertComponent,
     FooterComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
+    AlertComponent,
     FooterComponent
   ]
 })
