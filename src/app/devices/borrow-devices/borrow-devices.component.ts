9d4de8f5c573a8b69f489229ff9ff65aa9a2b23e
Iván L. Murillo <diskman199@gmail.com>
2021-06-23 09:58:13 -0500

    Listado de equipos disponibles

--- src/app/devices/borrow-devices/borrow-devices.component.ts
+++ src/app/devices/borrow-devices/borrow-devices.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { DevicesService } from 'src/app/services/devices.service';
+import { AlertService } from 'src/app/shared/alert';
+import { IconsAlert } from 'src/app/util/icons.alert';
 
 @Component({
   selector: 'app-borrow-devices',
@@ -7,9 +11,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BorrowDevicesComponent implements OnInit {
 
-  constructor() { }
+  public devices = null;
+  public icons = IconsAlert;
+
+  constructor(private devicesService  : DevicesService,
+              private router          : Router,
+              private alert           : AlertService) { }
 
   ngOnInit(): void {
+    this.loadDevices();
+  }
+
+  public deviceDetails(id : string){
+    this.router.navigate(['prestamos','empezar',id]);
+  }
+
+  private loadDevices(req = null) : void{
+    this.devicesService.loadDevices(req,'available')
+        .subscribe(resp=>{
+          if(resp){
+            this.devices = this.devicesService.getDevices();
+          }else{
+            this.alert.error(this.devicesService.getError());
+          }
+        },error=>{
+          this.alert.error(this.devicesService.getError());
+        });
+  }
+
+  public appyFilters($event){
+    this.loadDevices($event);
+  }
+
+  public resetFilters(){
+    this.loadDevices();
   }
 
 }
