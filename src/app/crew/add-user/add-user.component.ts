0afc3d121f0c4b6c542af79863acb34e91facdae
Iván L. Murillo <diskman199@gmail.com>
2021-07-09 09:02:00 -0500

    Diseño del formulario de  creación de usuarios

--- src/app/crew/add-user/add-user.component.ts
+++ src/app/crew/add-user/add-user.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AlertService } from 'src/app/shared/alert';
 
 @Component({
   selector: 'app-add-user',
@@ -6,10 +8,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-user.component.scss']
 })
 export class AddUserComponent implements OnInit {
+  
+  public form : FormGroup = Object.create(null);
 
-  constructor() { }
+  constructor(private fb    : FormBuilder,
+              private alert : AlertService) { }
 
   ngOnInit(): void {
+    this.form = this.fb.group({
+      nombre: ['',Validators.required],
+      username: ['',Validators.required],
+      email: ['', Validators.compose([Validators.required,Validators.email])],
+      password: ['', Validators.required],
+      posicion: ['', Validators.required],
+      temporal: ['']
+    });
   }
 
+  get(control : string) : AbstractControl{
+    return this.form.controls[control];
+  }
+
+  public getClass(ctrl : string){
+    if(!this.get(ctrl).touched){
+      return '';
+    }
+    return this.get(ctrl).valid ? 'is-valid' : 'is-invalid';
+  }
+
+  public next(){
+    this.get('temporal').setValue(this.get('password').value);
+    console.log(this.form.value);
+  }
 }
